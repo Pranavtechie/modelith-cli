2eb33cf434f32a1c50e7fad132b096732295f55b
Pranav <pranav.techiegeek@gmail.com>
2025-04-08 00:38:15 +0530

    working vite

--- src/frontend/src/routeTree.gen.ts
+++ src/frontend/src/routeTree.gen.ts
@@ -11,13 +11,27 @@
 // Import Routes
 
 import { Route as rootRoute } from './routes/__root'
+import { Route as SimilarityMatrixImport } from './routes/similarity-matrix'
 import { Route as IndexImport } from './routes/index'
+import { Route as DetailsPairImport } from './routes/details.$pair'
 
 // Create/Update Routes
+
+const SimilarityMatrixRoute = SimilarityMatrixImport.update({
+  id: '/similarity-matrix',
+  path: '/similarity-matrix',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const IndexRoute = IndexImport.update({
   id: '/',
   path: '/',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const DetailsPairRoute = DetailsPairImport.update({
+  id: '/details/$pair',
+  path: '/details/$pair',
   getParentRoute: () => rootRoute,
 } as any)
 
@@ -32,6 +46,20 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
+    '/similarity-matrix': {
+      id: '/similarity-matrix'
+      path: '/similarity-matrix'
+      fullPath: '/similarity-matrix'
+      preLoaderRoute: typeof SimilarityMatrixImport
+      parentRoute: typeof rootRoute
+    }
+    '/details/$pair': {
+      id: '/details/$pair'
+      path: '/details/$pair'
+      fullPath: '/details/$pair'
+      preLoaderRoute: typeof DetailsPairImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
@@ -39,32 +67,42 @@ declare module '@tanstack/react-router' {
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
+  '/similarity-matrix': typeof SimilarityMatrixRoute
+  '/details/$pair': typeof DetailsPairRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
+  '/similarity-matrix': typeof SimilarityMatrixRoute
+  '/details/$pair': typeof DetailsPairRoute
 }
 
 export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
+  '/similarity-matrix': typeof SimilarityMatrixRoute
+  '/details/$pair': typeof DetailsPairRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/'
+  fullPaths: '/' | '/similarity-matrix' | '/details/$pair'
   fileRoutesByTo: FileRoutesByTo
-  to: '/'
-  id: '__root__' | '/'
+  to: '/' | '/similarity-matrix' | '/details/$pair'
+  id: '__root__' | '/' | '/similarity-matrix' | '/details/$pair'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
+  SimilarityMatrixRoute: typeof SimilarityMatrixRoute
+  DetailsPairRoute: typeof DetailsPairRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
+  SimilarityMatrixRoute: SimilarityMatrixRoute,
+  DetailsPairRoute: DetailsPairRoute,
 }
 
 export const routeTree = rootRoute
@@ -77,11 +115,19 @@ export const routeTree = rootRoute
     "__root__": {
       "filePath": "__root.tsx",
       "children": [
-        "/"
+        "/",
+        "/similarity-matrix",
+        "/details/$pair"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/similarity-matrix": {
+      "filePath": "similarity-matrix.tsx"
+    },
+    "/details/$pair": {
+      "filePath": "details.$pair.tsx"
     }
   }
 }
