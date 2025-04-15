7e71f4366d8a5719290d618cfe60fb5be0b880ab
Pranav <pranav.techiegeek@gmail.com>
2025-04-15 10:51:14 +0530

    fix: config creation

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,24 +1,25 @@
 import envPaths from 'env-paths';
 import { join } from 'path';
-import { existsSync } from 'fs';
 
 const paths = envPaths('modelith', { suffix: '' });
 export const configPath = join(paths.config, 'config.json');
 export const dbPath = join(paths.data, 'modelith-db.sqlite');
 
 export async function loadEnv() {
+    await verifyConfigFile();
     const config = await Bun.file(configPath).json();
     Object.assign(process.env, config);
 }
 
 export async function verifyConfigFile() {
-    let configFile = Bun.file(configPath)
-    let configFileStatus = await configFile.exists();
-    if (!configFileStatus) {
+    const configFile = await Bun.file(configPath).exists()
+    const dbFile = await Bun.file(dbPath).exists()
+    if (configFile && dbFile) {
+        return true;
+    } else {
         await createConfigFile();
         return false;
     }
-    return true;
 }
 
 export async function createConfigFile() {
@@ -26,6 +27,7 @@ export async function createConfigFile() {
         DB_FILE_NAME: join(paths.data, 'modelith-db.sqlite'),
     };
 
-    await Bun.write(configPath, JSON.stringify(config, null, 2));
-    loadEnv()
+    await Bun.write(configPath, JSON.stringify(config, null, 2), { createPath: true });
+    await Bun.write(dbPath, '', { createPath: true })
+    await loadEnv()
 }
